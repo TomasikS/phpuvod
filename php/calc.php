2b6424a78c45eb2f541217a8b7763c8ddcf1ad83
studentsovy <stefan.tomasik@akademiasovy.sk>
2018-06-11 09:20:42 +0200

    project

--- php/calc.php
+++ php/calc.php
@@ -7,14 +7,14 @@
 
 
 <body>
-<h1> text</h1>
+<h1> Computing</h1>
 <?php 
 
 $a=$_GET ["val1"];
 $b=$_GET["val2"];
 $v=$_GET["check"];
 $ch=$_GET["stav"];
-echo $v;
+ 
 
 
 
@@ -31,24 +31,46 @@ else{
 
 $a=$_GET ["val1"];
 $b=$_GET["val2"];
-$c=$a+$b;
 
+echo "+";
+$c=$a+$b;
+echo "<br>";
 echo $c;
 $c=$a-$b;
+echo "-";
 echo $c;
 	}
 if($ch==2){
 	$a=$_GET ["val1"];
 $b=$_GET["val2"];
 $c=$a+$b;
-	$c=$a-$b;
-$c=$a*$b;
+echo "+";
+echo "<br>";
 echo $c;
+echo "<br>";
+
+	$c=$a-$b;
+	echo "-";
+	echo "<br>";
+echo $c;
+	
+	echo "<br>";
+	
+$c=$a*$b;
+	echo "*";
+	echo "<br>";
+echo $c;
+echo "<br>";
+
 if($b==0) echo "nulou sa nedelí";
 else {
 	
  $c=$a/$b;
+ echo "<br>";
+ echo "/";
+ echo "<br>";
  echo $c;
+ echo "<br>";
 	
 }
 }
